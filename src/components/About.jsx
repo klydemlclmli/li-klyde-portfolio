f4e4dcb70dfff1cf592fbc9f322a100a2253613f
klydemlclmli <klydemalcolmli@gmail.com>
2023-08-14 01:19:49 +0800

    Fixed padding of About section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -15,14 +15,14 @@ export default function About({sectionIds}) {
   const text = {transform: `translateY(${offsetY * -0.13}px)`};
 
   return (
-    <div id={sectionIds.about} className='base font-syne text-xl lg:text-2xl 2xl:text-[28px] text-white w-full py-[78px] px-14 xl:px-20 2xl:px-[130px] bg-black text-justify z-10 gap-[40px] md:gap-[70px] 2xl:gap-[80px]'>
+    <div id={sectionIds.about} className='base font-syne text-xl lg:text-2xl 2xl:text-[28px] text-white w-full pt-[78px] px-14 xl:px-20 2xl:px-[130px] bg-black text-justify z-10 gap-[40px] md:gap-[70px] 2xl:gap-[80px]'>
       
       <p style={text} className='aboutText font-besley text-4xl lg:text-5xl 2xl:text-[60px] font-medium underline underline-offset-8 pb-1 md:self-start'>
         ABOUT ME
       </p>
 
       <p style={text} className='aboutText md:w-[400px] lg:w-[450px] xl:w-[500px] 2xl:w-[540px] z-[1]'>
-        Hey there! I’m <b>Klyde</b>, an aspiring UI/UX Designer as of the creation of this portfolio.
+        Hey, there! I’m <b>Klyde</b>, an aspiring UI/UX Designer as of the creation of this portfolio.
       </p>
 
       <div className='base md:flex-row gap-[40px] xl:gap-[60px] 2xl:gap-[140px]'>
